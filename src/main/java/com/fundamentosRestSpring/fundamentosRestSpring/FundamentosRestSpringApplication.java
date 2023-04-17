d9a7356ecb739853410546f47df4d4beb7ebdcc2
mahernandez7 <miguel.hernandez-mu@uniminuto.edu.co>
2023-04-17 10:30:10 -0500

    fundamentos Spring Rest

---
+++ src/main/java/com/fundamentosRestSpring/fundamentosRestSpring/FundamentosRestSpringApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosRestSpring.fundamentosRestSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosRestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosRestSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosRestSpring/fundamentosRestSpring/FundamentosRestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosRestSpring.fundamentosRestSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosRestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
